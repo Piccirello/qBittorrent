2fd30cdb335e6f13a83c042c09528ef57b58eb77
Thomas Piccirello <thomas@piccirello.com>
2024-10-07 17:40:01 -0700

    Recalculate remaining size when priority changes
    This mimics the behavior of the GUI. Previously, we would have to wait for the API to respond with updated values before updating the 'remaining' size of folders.

--- src/webui/www/private/scripts/torrent-content.js
+++ src/webui/www/private/scripts/torrent-content.js
@@ -486,6 +486,7 @@ window.qBittorrent.TorrentContent ??= (() => {
         const updateComplete = function() {
             // we've finished recursing
             updateGlobalCheckbox();
+            torrentFilesTable.recalculateRemaining();
             torrentFilesTable.updateTable(true);
         };
 
